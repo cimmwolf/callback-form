3517031d147f856797b07bacbfc3202937218cdb
Denis Beliaev <cimmwolf@gmail.com>
2017-04-30 00:04:53 +0700

    Add success-message property

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,16 @@
 var gulp = require('gulp');
-var coffee = require('gulp-coffee');
-var vulcanize = require('gulp-vulcanize');
 var polyclean = require('polyclean');
 
-
-gulp.task('default', ['vulcanize'], function () {
-    gulp.src('callback-form.html')
+gulp.task('default', ['develop'], function () {
+    return gulp.src('callback-form.html')
         .pipe(polyclean.cleanCss())
         .pipe(polyclean.leftAlignJs())
         .pipe(polyclean.uglifyJs())
         .pipe(gulp.dest('./'));
-
-    return gulp.src('callback-form.html')
-        .pipe(gulp.dest('bower_components/callback-form-test'));
 });
 
-gulp.task('vulcanize', ['js-modules'], function () {
-    return gulp.src('src/*.html')
-        .pipe(vulcanize({
-            inlineScripts: true,
-            inlineCss: true,
-            excludes: ['bower_components/', 'iron-form/']
-        }))
+gulp.task('develop', function () {
+    return gulp.src('src/callback-form.html')
         .pipe(gulp.dest('./'))
-});
-
-gulp.task('js-modules', function () {
-    return gulp.src('src/*.coffee')
-        .pipe(coffee())
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('bower_components/callback-form-test'));
 });
