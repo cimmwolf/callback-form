17bec870532eee20bbbb1c46aa007e8932c6190f
cimmwolf <cimmwolf@gmail.com>
2019-05-19 00:36:58 +0400

    Polymer 3

---
+++ callback-form.js
@@ -0,0 +1,106 @@
+import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+import '@polymer/iron-form/iron-form.js';
+
+/**
+ * `callback-form`
+ * Callback form for sites
+ *
+ * @customElement
+ * @polymer
+ * @demo demo/index.html
+ */
+class CallbackForm extends PolymerElement {
+    static get template() {
+        return html`
+      <style>
+          :host { display: block; }
+      </style>
+      <iron-form id="callbackForm"
+                 on-iron-form-submit="_onSubmit"
+                 on-iron-form-error="_onError"
+                 on-iron-form-response="_onResponse"
+                 on-keydown="_checkForEnter">
+          <slot></slot>
+      </iron-form>
+    `;
+    }
+
+    static get is() {
+        return 'callback-form'
+    }
+
+    static get properties() {
+        return {
+            errorMessage: {
+                type: String,
+                value: 'Произошла ошибка. Попробуйте отправить вашу заявку позднее.'
+            },
+            submitMessage: {
+                type: String,
+                value: 'Заявка успешно отправлена!'
+            },
+            _submitBtn: Object,
+            _display: Object,
+            _formData: Object
+        }
+    }
+
+    ready() {
+        super.ready();
+        this._submitBtn = this.shadowRoot.querySelector('[type="submit"]');
+
+        if (!this._submitBtn) {
+            this._submitBtn = this.querySelector('button:not([type="button"])');
+        }
+        if (!this._submitBtn) {
+            console.error("Can't find submit button");
+        } else {
+            this._display = this._submitBtn.parentNode;
+        }
+    }
+
+    _onSubmit() {
+        var i, input, len, ref;
+        this._formData = this.$.callbackForm.serializeForm();
+        this._submitBtn.textContent = this._submitBtn.getAttribute('data-loading-text');
+        ref = this.querySelectorAll('[name]');
+        for (i = 0, len = ref.length; i < len; i++) {
+            ref[i].disabled = true;
+        }
+        this._submitBtn.disabled = true;
+    }
+
+    _onError() {
+        this._prepareForMessage();
+        this._display.textContent = this.errorMessage;
+        this._display.classList.add('alert-danger');
+        this.dispatchEvent(new CustomEvent('callback-form-sending-error', {detail: {formData: this._formData}}));
+    }
+
+    _onResponse(e) {
+        this._prepareForMessage();
+        if (e.detail.response && e.detail.response.message) {
+            this.submitMessage = e.detail.response.message;
+        }
+        this._display.textContent = this.submitMessage;
+        this._display.classList.add('alert-success');
+        this.dispatchEvent(new CustomEvent('callback-form-sending-success', {detail: {formData: this._formData}}));
+    }
+
+    _prepareForMessage() {
+        this._display.removeChild(this._submitBtn);
+        this._display.classList.add('alert');
+    }
+
+    submit() {
+        this.$.callbackForm.submit();
+    }
+
+    _checkForEnter(e) {
+        if (e.keyCode === 13) {
+            this.submit();
+        }
+    }
+}
+
+window.customElements.define('callback-form', CallbackForm);
