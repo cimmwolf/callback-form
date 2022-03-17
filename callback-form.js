1a10c022ea6572b479c060917a6f8b65214a1b45
pakart <obscurexdlol@gmail.com>
2022-03-17 14:40:32 +0300

    Non polymer callback-form (#2)
    * callback with iron-form
    * Сериализация и отправка формы
    * Модифицакия формы без использования polymer
    
    Co-authored-by: Pak Artem <pakart97@gmail.com>

--- callback-form.js
+++ callback-form.js
@@ -1,91 +1,51 @@
-import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
-import '@vistro/iron-form/iron-form.js';
+class CallbackForm extends HTMLElement {
 
-/**
- * `callback-form`
- * Callback form for sites
- *
- * @customElement
- * @polymer
- * @demo demo/index.html
- */
-class CallbackForm extends PolymerElement {
-  static get template() {
-    return html`<style>
-    :host { display: block; }
-</style>
-<iron-form id="callbackForm"
-           on-iron-form-submit="_onSubmit"
-           on-iron-form-presubmit="_onPreSubmit"
-           on-iron-form-error="_onError"
-           on-iron-form-response="_onResponse"
-           on-keydown="_checkForEnter">
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+  }
+
+  connectedCallback() {
+    const { shadowRoot } = this;
+
+    shadowRoot.innerHTML = `
+        <style>
+            :host { display: block; }
+        </style>
         <slot></slot>
-</iron-form>
-    `;
-  }
+        `;
 
-  static get is() {
-    return 'callback-form'
-  }
-
-  static get properties() {
-    return {
-      action: String,
-      method: {type: String, value: 'POST'},
-      errorMessage: {
-        type: String,
-        value: 'Произошла ошибка. Попробуйте отправить вашу заявку позднее.'
-      },
-      submitMessage: {
-        type: String,
-        value: 'Заявка успешно отправлена!'
-      },
-      _submitBtn: Object,
-      _display: Object,
-      _formData: Object,
-      _onAir: Boolean
-    }
-  }
-
-  ready() {
-    super.ready();
     this._submitBtn = this.querySelector('[type="submit"]');
+    this.errorMessage = 'Произошла ошибка. Попробуйте отправить вашу заявку позднее.';
+    this.submitMessage = 'Заявка успешно отправлена!'
 
     if (!this.querySelector('form') && this.action) {
       const form = document.createElement('form');
+      form.setAttribute('id', 'callbackForm');
       form.setAttribute('action', this.action);
       form.setAttribute('method', this.method);
       while (this.children.length > 0) {
         form.appendChild(this.children[0]);
       }
       this.appendChild(form);
+      this._callbackForm = form
     }
 
     if (!this._submitBtn) {
       this._submitBtn = this.querySelector('button:not([type="button"])');
     }
+
     if (!this._submitBtn) {
-      console.error("Can't find submit button");
     } else {
       this._display = this._submitBtn.parentNode;
-      this._submitBtn.addEventListener('click', (e) => {
-        e.preventDefault();
-        this.submit();
-      });
+      this._callbackForm.addEventListener('submit', (e) => { this._submitForm(e); })
     }
   }
 
-  _onSubmit() {
-    this._formData = this.$.callbackForm.serializeForm();
-    this._submitBtn.textContent = this._submitBtn.getAttribute('data-loading-text');
-    let ref = this.querySelectorAll('[name]');
-    for (let i = 0, len = ref.length; i < len; i++) {
-      ref[i].disabled = true;
-    }
-    this._submitBtn.disabled = true;
-  }
+  get action() { return this.getAttribute('action') }
+  get method() { return 'POST' }
 
+  /* Перед отправкой */
   _onPreSubmit(e) {
     if (this._onAir) {
       e.stopPropagation();
@@ -95,40 +55,157 @@ class CallbackForm extends PolymerElement {
     this._onAir = true;
   }
 
-  _onError() {
-    this._prepareForMessage();
-    this._display.textContent = this.errorMessage;
-    this._display.classList.add('alert-danger');
-    this.dispatchEvent(new CustomEvent('callback-form-sending-error', {detail: {formData: this._formData}}));
+  /* При отправке */
+  _onSubmit() {
+    this._submitBtn.textContent = this._submitBtn.getAttribute('data-loading-text');
+    let ref = this.querySelectorAll('[name]');
+    for (let i = 0, len = ref.length; i < len; i++) {
+      ref[i].disabled = true;
+    }
+    this._submitBtn.disabled = true;
   }
 
-  _onResponse(e) {
+  /* При успехе */
+  _onSuccess(msg) {
     this._prepareForMessage();
-    if (e.detail.response && e.detail.response.message) {
-      this.submitMessage = e.detail.response.message;
+    if (msg !== '') {
+      this.submitMessage = msg;
     }
     this._display.textContent = this.submitMessage;
     this._display.classList.add('alert-success');
-    this.dispatchEvent(new CustomEvent('callback-form-sending-success', {detail: {formData: this._formData}}));
   }
 
+
+  /* При ошибке */
+  _onError(msg) {
+    this._prepareForMessage();
+    this._display.textContent = this.errorMessage;
+    this._display.classList.add('alert-danger');
+  }
+
+  /* Подготовка к сообщению */
   _prepareForMessage() {
     this._onAir = false;
-    if(this._submitBtn) {
+    if (this._submitBtn) {
       this._display.removeChild(this._submitBtn);
     }
     this._display.classList.add('alert');
   }
 
-  submit() {
-    this.$.callbackForm.submit();
+  /* Вызов сабмита */
+  _submitForm(e) {
+    e.preventDefault();
+    if(this._validate()){
+      this._onPreSubmit(e);
+      this._onSubmit();
+      const json = this._serializeForm();
+      this._makeAjaxRequest(json).then((res)=>{
+        if(res.status === 200) {
+          this._onSuccess(res.statusText);
+        }else{
+          this._onError();
+        }
+      });
+    }
   }
 
-  _checkForEnter(e) {
-    if (e.keyCode === 13) {
-      this.submit();
+  //Валидация
+  _validate() {
+
+    if (!this._callbackForm) {
+      return false;
+    }
+
+    let valid = this._callbackForm.checkValidity();
+    let elements = this. _getSubmittableElements();
+
+    for (let el, i = 0; el = elements[i], i < elements.length; i++) {
+      let validatable = (el);
+      if (validatable.validate) {
+        valid = !!validatable.validate() && valid;
+      }
+    }
+    return valid;
+  }
+
+  //Сериализация формы
+  _serializeForm() {
+    let elements = this._getSubmittableElements();
+    let json = {};
+    for (let i = 0; i < elements.length; i++) {
+      let values = this._serializeElementValues(elements[i])
+      for (let v = 0; v < values.length; v++) {
+        this._addSerializedElement(json, elements[i].name, values[v]);
+      }
+    }
+    return json;
+  }
+
+  /* аjax */
+  async _makeAjaxRequest(data) {
+    const response = await fetch(this.action, {
+      method: this.method,
+      body: data
+    });
+
+    return await response;
+  }
+
+  /* Блок функций для сборки данных формы */
+  _getSubmittableElements() {
+    return this._callbackForm.querySelectorAll('[name]');
+  }
+
+  _serializeElementValues(element) {
+    const tag = element.tagName.toLowerCase();
+
+    if (tag === 'button' ||
+        (tag === 'input' &&
+            (element.type === 'submit' || element.type === 'reset'))) {
+      return [];
+    }
+
+    if (tag === 'select') {
+      return this._serializeSelectValues(element);
+    } else if (tag === 'input') {
+      return this._serializeInputValues(element);
+    } else {
+      if (element['_hasIronCheckedElementBehavior'] && !element.checked)
+        return [];
+      return [element.value];
+    }
+  }
+
+  _serializeSelectValues(element) {
+    let values = [];
+
+    for (let i = 0; i < element.options.length; i++) {
+      if (element.options[i].selected) {
+        values.push(element.options[i].value)
+      }
+    }
+    return values;
+  }
+
+  _serializeInputValues(element) {
+    let type = element.type.toLowerCase();
+    if (((type === 'checkbox' || type === 'radio') && !element.checked) ||
+        type === 'file') {
+      return [];
+    }
+    return [element.value];
+  }
+
+  _addSerializedElement(json, name, value) {
+    if (json[name] === undefined) {
+      json[name] = value;
+    } else {
+      if (!Array.isArray(json[name])) {
+        json[name] = [json[name]];
+      }
+      json[name].push(value);
     }
   }
 }
 
-window.customElements.define('callback-form', CallbackForm);
+customElements.define('callback-form', CallbackForm);
